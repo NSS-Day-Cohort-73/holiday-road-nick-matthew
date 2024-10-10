b3c511a8cbf5ea780ad5d0690c8f91dbcc2fde9b
herringvoices <nalherring@gmail.com>
2024-10-10 15:27:37 -0500

    Still wrong but not quite so wrong now.

--- scripts/weather/RenderOtherDropdown.js
+++ scripts/weather/RenderOtherDropdown.js
@@ -2,15 +2,17 @@ export const renderOtherDropdown = async (state, type) =>{
 const response = await fetch(`http://holidayroad.nss.team/${type}`)
 const responseArray = await response.json()
 const filteredArray = responseArray.filter(item => state.includes(item.state))
-const html =' <select id="eaterie-dropdown">'
-html += filteredArray.map(item =>{`
+let html =' <select id="eaterie-dropdown">'
+html += filteredArray.map(item => `
                     <option
                     data-name="${item.name}"
                     data-parkid="${item.id}">
-                    Option 1
+                    ${item.name}
                     </option>
-                </select>
-`})
+                
+`).join('')
+html += '</select>'
+return html
 
 }
 
