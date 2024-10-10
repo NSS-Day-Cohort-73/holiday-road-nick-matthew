837675611eb42ad2f59cc895ea9f6f11eda0f64b
herringvoices <nalherring@gmail.com>
2024-10-10 15:34:01 -0500

    Added default option tag

--- scripts/weather/RenderOtherDropdown.js
+++ scripts/weather/RenderOtherDropdown.js
@@ -2,11 +2,12 @@ export const renderOtherDropdown = async (state, type) =>{
 const response = await fetch(`http://holidayroad.nss.team/${type}`)
 const responseArray = await response.json()
 const filteredArray = responseArray.filter(item => state.includes(item.state))
-let html =' <select id="eaterie-dropdown">'
+let html =' <select id="eaterie-dropdown"><option value="0" data-id="0">Make a selection.</option>'
 html += filteredArray.map(item => `
                     <option
                     data-name="${item.name}"
-                    data-parkid="${item.id}">
+                    data-id="${item.id}"
+                    value="${item.id}">
                     ${item.name}
                     </option>
                 
